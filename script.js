f6ca455790884f1dcf52a09f5e778b0304bfa9eb
yusuftrg13 <132002974+yusuftrg13@users.noreply.github.com>
2023-04-27 21:07:14 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const arrows=document.querySelectorAll(".arrow");
+const movieLists=document.querySelectorAll(".movie-list");
+arrows.forEach((arrow,i)=>{
+    arrow.addEventListener("click",function(){
+        movieLists[i].style.transform = "translateX(-300px)";
+    });
+});
